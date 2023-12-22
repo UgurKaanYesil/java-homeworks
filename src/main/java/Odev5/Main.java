bf81f09a0b365eca6b4f6a2f7728a54ab3c43f02
Uğur Kaan Yeşil <ugurkan.yesil@gmail.com>
2023-12-22 16:42:37 +0300

    scanner added for worked days data

--- src/main/java/Odev5/Main.java
+++ src/main/java/Odev5/Main.java
@@ -1,13 +1,22 @@
 package Odev5;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Employee accountingEmployee = new Employee("Uğur Kaan", "Yeşil", 28);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter worked days: ");
+        int workedDays = scanner.nextInt();
+
+        Employee accountingEmployee = new Employee("Uğur Kaan", "Yeşil", workedDays);
 
         double salary = accountingEmployee.calculateSalary();
 
         System.out.println("First Name: " + accountingEmployee.getFirstName() + ", Last Name: " + accountingEmployee.getLastName());
         System.out.println("Worked Days: " + accountingEmployee.getWorkedDays());
         System.out.println("Salary: " + salary + " TL");
+
+        scanner.close();
     }
 }
